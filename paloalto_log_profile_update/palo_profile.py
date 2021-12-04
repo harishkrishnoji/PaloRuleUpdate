c6fbecb0d61b63487212560f48261caa256740e0
Harish Krishnoji - 10105547 <harish.krishnoji@fiserv.com>
2021-12-04 00:59:19 +0000

    Fix log-start-end

--- paloalto_log_profile_update/palo_profile.py
+++ paloalto_log_profile_update/palo_profile.py
@@ -71,12 +71,7 @@ def display_results(sec_rules):
             (not rule.log_setting)
             or (not rule.group)
             or (not isinstance(rule.group, list))
-            or (
-                rule.log_setting != "default"
-                or rule.log_start
-                or not rule.log_end
-                and (rule.tag and "No-Log" not in rule.tag)
-            )
+            or (rule.log_setting != "default" and (rule.tag and "No-Log" not in rule.tag))
             or (rule.group[0] != "default" or rule.group[0] != "scanner" or (rule.tag and "No-Log" not in rule.tag))
             and (rule.action == "allow")
         ):
